85beadddd578c714a6b378449d089fbabfc447f4
redroshan <roshanssj556@gmail.com>
2017-01-29 19:54:01 +0530

    beginning

---
+++ footprint.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env  python2
+
+
+import time
+print "Welcome to SHARPEYE"
+
+time.sleep(2)
+
+
+print ""
+print ""
+print "***********************************************"
+print "+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+"
+
+x='''
+Press  1  To  check my System IP  address  :  
+Press  2  To  check my System MAC  address  :  
+Press  3  To  check my System OS Type  :  
+Press  4  To  check my System OS version   :  
+Press  5  To  check my system Configuration   : 
+'''
+
+print x
